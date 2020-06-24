15f5a04f10d45cb49d9bc435eb2bc83859870147
Peter Cassell <pc0122@my.bristol.ac.uk>
2020-06-24 23:59:51 +0100

    first

---
+++ HttpClientDemo/Models/WeatherForecast.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpClientDemo.Models
+{
+    public class WeatherForecast
+    {
+
+    }
+
+    public class Rootobject
+    {
+        public DayForecastModel[] consolidated_weather { get; set; }
+        public DateTime time { get; set; }
+        public DateTime sun_rise { get; set; }
+        public DateTime sun_set { get; set; }
+        public string title { get; set; }
+        public string timezone { get; set; }
+    }
+
+    public class DayForecastModel
+    {
+        public string weather_state_name { get; set; }
+        public DateTime created { get; set; }
+        public string applicable_date { get; set; }
+        public float min_temp { get; set; }
+        public float max_temp { get; set; }
+        public float the_temp { get; set; }
+        public float wind_speed { get; set; }
+        public float wind_direction { get; set; }
+        public float air_pressure { get; set; }
+        public int humidity { get; set; }
+        public float visibility { get; set; }
+        public int predictability { get; set; }
+    }
+
+}
---
+++ README.md.txt
@@ -0,0 +1,3 @@
+A Basic HTTP Demo
+
+https://www.metaweather.com/api/location/2471217/
